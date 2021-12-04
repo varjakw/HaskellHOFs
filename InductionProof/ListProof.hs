63400eba3a7dc8020bd8853d19ae43d768fa06ff
Varjak Wolfe <78870995+varjakw@users.noreply.github.com>
2021-12-04 17:33:45 +0000

    Create ListProof.hs

---
+++ InductionProof/ListProof.hs
@@ -0,0 +1,48 @@
+{-We will prove length (xs++ys) = length xs + length ys
+
+Lists are defined inductively as either [] or x:xs, where xs is a pre-existing list.
+
+We do an inductive proof on xs
+
+P(xs) ≙ length (xs++ys) = length xs + length ys
+
+Why not do induction with ys instead?
+-}
+
+{-Base Case-}
+P([])
+  = "expand P"
+ 
+length ([]++ys) = length [] + length ys
+  = "Defs of ++ and length"
+  
+length ys = 0 + length ys
+  = "arithmetic"
+  
+length ys = length ys
+  = "reflexivity of ="
+  
+True
+
+{-Inductive Step
+Assume P(xs) i.e. kength (xs++ys) = length xs + length ys
+
+Show P(x:xs)
+-}
+
+P(x:xs)
+  = "expand P"
+ 
+length ((x:xs)++ys) = length (x:xs) + length ys
+  = "Defs of ++ and length"
+  
+length (x:(xs++ys)) = (1 + length xs) + length ys
+  = "Defs of length, + is associative"
+  
+1 + length (xs++ys) = 1 + (length xs + length ys)
+  = "arithmetic"
+  
+length (xs++ys) = length xs + length ys
+  = "by induction hypothesis"
+  
+True
